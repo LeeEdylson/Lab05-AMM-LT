e410daf020bc2768d6a7bab598b08bd77fee5294
LeeEdylson <66333627+LeeEdylson@users.noreply.github.com>
2021-09-14 20:43:35 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio03/Ejercicio03/ImageResourceExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Internals;
+
+namespace Ejercicio03
+{
+    [Preserve(AllMembers = true)]
+    [ContentProperty(nameof(Source))]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+                return null;
+
+            var imageSource = ImageSource.FromResource(Source,
+                typeof(ImageResourceExtension).GetType().Assembly);
+            return imageSource;
+        }
+    }
+}
